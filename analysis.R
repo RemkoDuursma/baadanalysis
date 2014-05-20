261d90156e226d5552ab935d411f6c6cae282794
Remko Duursma <remkoduursma@gmail.com>
2014-05-20 12:56:04 +1000

    stem_leaf_scaling compiles

--- analysis.R
+++ analysis.R
@@ -4,8 +4,16 @@ source("R/analysis_utils.R")
 source("R/loadPackages.R")
 source("R/plotting.R")
 
-
+# Meta data summaries, tables, world map
 knitFromHere("Rnw/metadata_tables.Rnw")
-  
+
+# stem-leaf scaling
+knitFromHere("Rnw/stem_leaf_scaling.Rnw")
+
+# pipe model plot from Daniel
+source("R/pipe.R")
+
+
+
 # BAAD
 baad <- readRDS("data/baad.rds")$data
