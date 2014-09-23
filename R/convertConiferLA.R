224298d890398a5766a5d11922d60f50a7d928b0
Remko Duursma <remkoduursma@gmail.com>
2014-09-24 09:41:08 +1000

    Fixed convertConiferLA for new methods definitions

--- R/convertConiferLA.R
+++ R/convertConiferLA.R
@@ -3,32 +3,33 @@
 convertConiferLA <- function(baad){
   
   # One sided total leaf area (half total surface area)
-  # to projjected area. Average of non-pinus species in Barclay & Goodman 2000
+  # to projjected area. Average of species in Barclay & Goodman 2000
   # lambda1 <- c(0.873, 0.92, 0.879, 0.864, 0.839); mean(1/lambda1) # B & G
   # For Pinus this is typically assumed to equal pi/2 (following Grace 1987 NZ J For)
   ola_pla <- 1.14
   
   conv_pine <- function(x, method){
-    if(method %in% c("","?"))
+    if(method %in% c("","?","ax"))
       method <- "unknown"
     
+    cv <- 1/0.778
     switch(method, 
-           r = x,
-           r2 = x/pi,
-           r4 = x/pi,
-           r3 = x/(pi/2),
+           a4 = x,
+           a5 = x/(2 * cv),
+           a6 = x/ cv,
+           a7 = x/(2 * cv),
            unknown = x
            )
   }
   conv_nonpine <- function(x, method){
-    if(method %in% c("","?"))
+    if(method %in% c("","?","ax"))
       method <- "unknown"
     
     switch(method, 
-           r = x,
-           r2 = x/(2 * ola_pla),
-           r4 = x/(2 * ola_pla),
-           r3 = x/ola_pla, 
+           a4 = x,
+           a5 = x/(2 * ola_pla),
+           a6 = x/ola_pla, 
+           a7 = x/(2 * ola_pla),
            unknown = x
     )
   }
@@ -37,7 +38,7 @@ convertConiferLA <- function(baad){
     
     if(pft %in% c("DA","EA"))return(x)
     
-    if(grepl("Pinus", species))
+    if(grepl("Pinus", species,ignore.case=TRUE))
       conv_pine(x, method)
     else
       conv_nonpine(x, method)
@@ -45,7 +46,7 @@ convertConiferLA <- function(baad){
   conv <- Vectorize(convf)
 
   alfmeth <- baad$methods[,c("studyName","a.lf")]
-  alfmeth$method_alf <- str_extract(alfmeth$a.lf,"r[0-9]{0,1}")
+  alfmeth$method_alf <- str_extract(alfmeth$a.lf,"a[4-7]{1}")
   alfmeth$method_alf[is.na(alfmeth$method_alf)] <- ""
   baad$data <- merge(baad$data, alfmeth[,c("studyName","method_alf")],all=T)
   
