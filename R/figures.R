18812f202382fa8547eb0da4baac2e34cf84b863
Remko Duursma <remkoduursma@gmail.com>
2014-09-28 21:55:10 +1000

    figures update (for Combio etc)

--- R/figures.R
+++ R/figures.R
@@ -9,6 +9,43 @@ linecols <- c("deepskyblue3","firebrick2","chartreuse3")
 palette(Cols)
 
 
+# These need to be merged :)
+Legend <- function(where){
+  legend(where, c("Decid. Angio.", "Evergr. Angio.", "Evergr. Gymno."),
+         pch=19, col=Cols, bty='n', cex=1, pt.cex=1)
+}
+
+
+# Root-shoot
+to.pdf({
+  par(mar=c(5,5,2,2), cex.lab=1.2)
+  smoothplotbypft(log10(m.rt), log10(m.so), dataset2, 
+                  xlab=expression(Root~mass~(kg)),
+                  ylab=expression(Aboveground~mass~(kg)), cex=0.6,pointcols=transCols,linecols=linecols)
+  
+  abline(0,1)
+  Legend("topleft")
+}, filename="output/figures/mrt_mso_bypft.pdf", width=6, height=5)
+
+
+
+# pipe model plots
+to.pdf({
+  par(mar=c(5,5,2,2), cex.lab=1.2)
+  smoothplotbypft(log10(a.stba2), log10(m.lf), dataset2, xlab=expression(Basal~stem~area~~(m^2)),
+                  ylab=expression(Plant~leaf~mass~(kg)), cex=0.6,pointcols=transCols,linecols=linecols)
+  Legend("topleft")
+}, filename="output/figures/mlf_astba2_bypft.pdf", width=6, height=5)
+
+to.pdf({
+  par(mar=c(5,5,2,2), cex.lab=1.2)
+  smoothplotbypft(log10(a.stba2), log10(a.lf), dataset2, xlab=expression(Basal~stem~area~~(m^2)),
+                  linecols=linecols, pointcols=transCols,
+                  ylab=expression(Plant~leaf~area~(m^2)), cex=0.6)
+  Legend("topleft")
+}, filename="output/figures/alf_astba2_bypft.pdf", width=6, height=5)
+
+
 # Pipe model
 to.pdf({
 par(cex.axis=0.85, mfrow=c(1,2), mar=c(5,5,1,1), cex=1.1)
@@ -52,36 +89,6 @@ meansbypft("lmlf_astba2","lalf_astba2", "pftlong",
 }, filename="output/figures/mlf_alf_astbaest_pftlongmeans.pdf", width=8, height=4)
 
 
-
-# LMF and LAR plot
-Legend <- function(){
-  legend("bottomleft", c("Decid. Angio.", "Evergr. Angio.", "Evergr. Gymno."),
-         pch=19, col=Cols, bty='n', cex=1, pt.cex=1)
-}
-
-
-# LMF
-to.pdf({
-  par(mar=c(5,5,2,2), cex.axis=0.9, cex.lab=1.1)
-  gamplotandpred(dataset2, "pft", "lmlf_mso", plotwhich=1, 
-                 lineCols=linecols, pointCols=transCols,vlines=FALSE,legend=FALSE,
-                 xlab="Plant height (m)",
-                 ylab=expression("Leaf mass / aboveground biomass"~~(kg~kg^-1)))
-  Legend()
-}, filename="output/figures/LMF_pft_lines.pdf", width=7, height=4.5)
-
-
-# LAR
-to.pdf({
-  par(mar=c(5,5,2,2), cex.axis=0.9, cex.lab=1.1)
-  gamplotandpred(dataset2, "pft", "lalf_mso", plotwhich=1, 
-                 lineCols=linecols, pointCols=transCols,vlines=FALSE,legend=FALSE,
-                 xlab="Plant height (m)",
-                 ylab=expression("Leaf area / aboveground biomass"~~(m^2~kg^-1)))
-  Legend()
-
-}, filename="output/figures/LAR_pft_lines.pdf", width=7, height=4.5)
-
 # Leaf mass / stem area
 to.pdf({
   par(mfrow=c(3,1), mar=c(0,0,0,0), oma=c(5,5,2,2), las=1)
@@ -96,36 +103,95 @@ to.pdf({
 # Leaf area / stem area
 to.pdf({
   par(mfrow=c(3,1), mar=c(0,0,0,0), oma=c(5,5,2,2), las=1)
-histbypft(lalf_astba2, pft, dataset2, xaxis=3,legend.cex=1,col=Cols,
-          xlab=expression("Leaf area / basal stem area"~(m^2~m^-2)),
-          legend.text=c("Decid. Angio.",
-                        "Evergr. Angio.",
-                        "Evergr. Gymno."))
+  histbypft(lalf_astba2, pft, dataset2, xaxis=3,legend.cex=1,col=Cols,
+            xlab=expression("Leaf area / basal stem area"~(m^2~m^-2)),
+            legend.text=c("Decid. Angio.",
+                          "Evergr. Angio.",
+                          "Evergr. Gymno."))
 }, filename="output/figures/lalf_astba2_hist_bypft.pdf", width=4, height=8)
+
+
+
+# Leaf mass, woody mass
+# LMF scaling
+mstmlf_ht <- function(){
   
-
-
-
-# pipe model plots
-Legend2 <- function(){
-  legend("topleft", c("Decid. Angio.", "Evergr. Angio.", "Evergr. Gymno."),
-         pch=19, col=Cols, bty='n', cex=1.1, pt.cex=1)
+  par(mfrow=c(1,3), mar=c(0,0,0,0), oma=c(5,5,2,2))
+  labels <- c("Decid. Angio.", "Evergr. Angio.", "Evergr. Gymno.")
+  
+  for(i in 1:3){
+    p <- levels(dataset2$pft)[i]
+    dat <- dataset2[dataset2$pft == p,]
+    
+    with(dat, plot(log10(h.t), log10(m.st), pch=16,cex=0.5,
+                   xlim=log10(c(0.01,105)), ylim=c(-6,6),
+                   xlab="Plant height (m)",
+                   ylab="Woody or foliage mass (kg)",
+                   col=alpha("brown",0.5),axes=FALSE))
+    with(dat, points(log10(h.t), log10(m.lf), pch=16,cex=0.5,
+                     col=alpha("forestgreen",0.5)))
+    
+    magaxis(side=1, unlog=1, labels=TRUE)
+    magaxis(side=2, unlog=2, labels= i == 1)
+    box()
+    legend("topleft", labels[i], bty='n', cex=1.2, text.font=3)
+    if(i == 1){
+      legend(-2,4.5, c("Leaf","Woody abvgr."), pch=19, 
+             col=c("forestgreen","brown"), bty='n', pt.cex=1, cex=1.2)
+    }
+  }
+  
+  mtext(side=1, text="Plant height (m)", line=3, outer=TRUE)
+  mtext(side=2, text="Leaf or woody biomass (kg)", line=3, outer=TRUE)
 }
+to.pdf(mstmlf_ht(), width=9, height=4,
+       filename="output/figures/mlfmst_byht_pft.pdf")
+
+
+
+# LMF
+to.pdf({
+  par(mar=c(5,5,2,2), cex.axis=0.9, cex.lab=1.1)
+  g <- gamplotandpred(dataset2, "pft", "lmlf_mso", plotwhich=1, 
+                 lineCols=linecols, pointCols=transCols,vlines=FALSE,legend=FALSE,
+                 xlab="Plant height (m)",
+                 ylab=expression("Leaf mass / aboveground biomass"~~(kg~kg^-1)))
+  Legend("bottomleft")
+}, filename="output/figures/LMF_pft_lines.pdf", width=7, height=4.5)
 
 to.pdf({
-  par(mar=c(5,5,2,2), cex.lab=1.2)
-  smoothplotbypft(log10(a.stba2), log10(m.lf), dataset2, xlab=expression(Basal~stem~area~~(m^2)),
-                  ylab=expression(Plant~leaf~mass~(kg)), cex=0.6,pointcols=transCols,linecols=linecols)
-  Legend2()
-}, filename="output/figures/mlf_astba2_bypft.pdf", width=6, height=5)
-  
+  par(mfrow=c(1,2), cex.main=0.9, mar=c(5,5,1,1))
+plotg(g,1,xlab=expression(Specific~leaf~mass~(kg~m^-2)),
+      ylab=expression(Leaf~mass/Aboveground~mass~(kg~kg^-1)),
+      xlim=c(0,0.2), ylim=c(0,0.5), main="Height = 1m")
+      
+plotg(g,2,xlab=expression(Specific~leaf~mass~(kg~m^-2)),
+      ylab=expression(Leaf~mass/Aboveground~mass~(kg~kg^-1)),
+      xlim=c(0,0.2), ylim=c(0,0.2), main="Height = 10m")
+}, filename="output/figures/LMF_estfromgam_bypftandlma.pdf", width=8, height=4)
+
+
+
+# LAR
 to.pdf({
-  par(mar=c(5,5,2,2), cex.lab=1.2)
-  smoothplotbypft(log10(a.stba2), log10(a.lf), dataset2, xlab=expression(Basal~stem~area~~(m^2)),
-                  linecols=linecols, pointcols=transCols,
-                  ylab=expression(Plant~leaf~area~(m^2)), cex=0.6)
-  Legend2()
-}, filename="output/figures/alf_astba2_bypft.pdf", width=6, height=5)
+  par(mar=c(5,5,2,2), cex.axis=0.9, cex.lab=1.1)
+  g2 <- gamplotandpred(dataset2, "pft", "lalf_mso", plotwhich=1, 
+                 lineCols=linecols, pointCols=transCols,vlines=FALSE,legend=FALSE,
+                 xlab="Plant height (m)",
+                 ylab=expression("Leaf area / aboveground biomass"~~(m^2~kg^-1)))
+  Legend("bottomleft")
+
+}, filename="output/figures/LAR_pft_lines.pdf", width=7, height=4.5)
 
 
+to.pdf({
+  par(mfrow=c(1,2), cex.main=0.9, mar=c(5,5,1,1))
+plotg(g2,1,xlab=expression(Specific~leaf~mass~(kg~m^-2)),
+      ylab=expression(Leaf~area/Aboveground~mass~(m^2~kg^-1)),
+      xlim=c(0,0.2), ylim=c(0,10),  main="Height = 1m")
+
+plotg(g2,2,xlab=expression(Specific~leaf~mass~(kg~m^-2)),
+      ylab=expression(Leaf~area/Aboveground~mass~(m^2~kg^-1)),
+      xlim=c(0,0.2),  ylim=c(0,1), main="Height = 10m")
+}, filename="output/figures/LAR_estfromgam_bypftandlma.pdf", width=8, height=4)
 
