b9b4f8ea3e2911fab728072e4adb95f851408c2b
Remko Duursma <remkoduursma@gmail.com>
2014-10-14 15:55:16 +1100

    dependencies update (duplicates removed, added lsmeans and maptools)

--- dependencies.R
+++ dependencies.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env Rscript
 
-pkgs <- c("xtable","lmerTest","maps","mapdata","gdata","knitr","smatr","scales","xtable","magicaxis","rmarkdown","dplyr","sciplot","stringr","epade","multcomp","doBy","mgcv","reshape2","lme4","Hmisc","hexbin","lattice","grid","stringr","raster","dismo","XML","rgdal")
+pkgs <- c("lmerTest","maps","mapdata","maptools","gdata","knitr","smatr","scales",
+          "xtable","magicaxis","rmarkdown","dplyr","sciplot","epade",
+          "multcomp","doBy","mgcv","reshape2","lme4","Hmisc","hexbin",
+          "lattice","grid","stringr","raster","dismo","XML","rgdal","lsmeans")
 
 pkgs.missing <- setdiff(pkgs, rownames(installed.packages()))
 install.packages(pkgs.missing)
