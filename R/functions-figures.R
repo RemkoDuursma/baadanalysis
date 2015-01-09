82972df19923910201faafe10c8ee3556266aa2d
Daniel Falster <daniel.falster@mq.edu.au>
2015-01-09 11:02:38 +1100

    Extend to handle log-scaled axes

--- R/functions-figures.R
+++ R/functions-figures.R
@@ -20,12 +20,18 @@ alpha <- function (colour, alpha = NA) {
 
 
 # Simple function for placing labels on a figure.
-plotlabel <- function(txt, where, inset=0.08, inset.x=inset, inset.y=inset,...){
+plotlabel <- function(txt, where, inset=0.08, inset.x=inset, inset.y=inset, log.y=FALSE, log.x=FALSE,...){
   u <- par()$usr
   if(grepl("left",where))x <- u[1] + inset.x*(u[2]-u[1])
   if(grepl("right",where))x <- u[2] - inset.x*(u[2]-u[1])
   if(grepl("bottom",where))y <- u[3] + inset.y*(u[4]-u[3])
   if(grepl("top",where))y <- u[4] - inset.y*(u[4]-u[3])
+
+  if(log.x)
+    x <- 10^x
+
+  if(log.y)
+    y <- 10^y
 
   text(x,y,txt,...)
 }
